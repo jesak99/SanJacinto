1ffe9b321f3e6be06cde6f09fc329b415c0e9853
jesak99 <020499az@gmail.com>
2023-01-24 21:17:31 -0600

    Version final

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -2,14 +2,15 @@ importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-app-compat.js")
 importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-    projectId: 'san-jacinto-amilpas',
-    appId: '1:147714289791:web:5dadc25146c8151c464f4a',
+    projectId: 'san-jacinto-amilpas-web',
+    appId: '1:309881000808:web:7fcbff524f3e241c4a7cdd',
     databaseURL: 'https://san-jacinto-amilpas-default-rtdb.firebaseio.com',
-    storageBucket: 'san-jacinto-amilpas.appspot.com',
+    storageBucket: 'san-jacinto-amilpas-web.appspot.com',
     locationId: 'us-central',
-    apiKey: 'AIzaSyAcKSdWOLINLU9eSs3AuUEAPUYCs-ZGbqM',
-    authDomain: 'san-jacinto-amilpas.firebaseapp.com',
-    messagingSenderId: '147714289791',
-    measurementId: 'G-HRG2X7P41D',
+    apiKey: 'AIzaSyDfSmJk_n3D8vAZGk_afw3QN1hR4dcINX8',
+    authDomain: 'san-jacinto-amilpas-web.firebaseapp.com',
+    messagingSenderId: '309881000808',
+    measurementId: 'G-QW20DH6HE4',
+    vapidKey: "BF0xGjrrCmUuPimbl7TBJEDOBmKadBpBuh4LczkydsbBKj4ODya-Y1XjVVB-HdL6AJiUiNvwuVjjogg7fzE15MU"
 });
 const messaging = firebase.messaging();
